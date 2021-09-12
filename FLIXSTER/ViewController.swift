5916466c27fd319c0bd9e17e8e3c8865c955b282
Daanish Goyal <dg5052@gmail.com>
2021-09-12 15:19:38 -0400

    Initial Commit

---
+++ FLIXSTER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FLIXSTER
+//
+//  Created by Daanish Goyal on 12/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
